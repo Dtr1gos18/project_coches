c3a825fe243aa3f896290ab86206d929d7dea18d
Dtr1gos18 <diego.trigos1805@gmail.com>
2023-08-09 17:13:34 -0500

    se crea un package llamdo service dentro del package domain y dos clases, una clase interfaz donde estan los metodos del modelo del negocio y una clase normal donde hacemos uso de estos metodos que nos serviran como servicios

---
+++ src/main/java/com/project/coches/domain/service/IBrandCarService.java
@@ -0,0 +1,35 @@
+package com.project.coches.domain.service;
+
+import com.project.coches.domain.pojo.BrandCarPojo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IBrandCarService {
+
+    /**
+     * Develve una lista con todas las marcas de coches
+     * @return lista con marcas de coches
+     */
+    List<BrandCarPojo> getAll();
+
+    /**
+     * Devuelve una marca de coche dado su id, se usa para el uso de los nulos e impedir que el programa se rompa
+     * @param id de marca coche
+     * @return Optional del marca de coche encontrado
+     */
+    Optional<BrandCarPojo> getBrandCar(Integer id);
+
+    /**
+     * Guarda una nueva marca coche
+     * @param newBrandCar marca coche a guarda
+     * @return marca coche guardada
+     */
+    BrandCarPojo save(BrandCarPojo newBrandCar);
+
+    /**
+     * Elimina una marca coche dado su id
+     * @param idBrandCar id de la marca coche a eliminar
+     */
+    boolean delete(Integer idBrandCar);
+}
