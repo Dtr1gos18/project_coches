fb234c6fa255aa65b6e7e367de79abc99587fe48
Dtr1gos18 <diego.trigos1805@gmail.com>
2023-08-14 16:05:17 -0500

    Se crean los dto de carPurchase y de purchase

---
+++ src/main/java/com/project/coches/domain/dto/CarPurchaseResponseDto.java
@@ -0,0 +1,17 @@
+package com.project.coches.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//Dto de la consulta de los carros de una compra
+@Getter @Setter
+public class CarPurchaseResponseDto {
+
+    private String referenceCar;
+
+    private Integer quantity;
+
+    private Integer totalPrice;
+
+}
+
